72c3f5a1c696ea3bdbf3b42339c0bff0a1a9cb8c
unknown <alelybitun.school@gmail.com>
2025-09-01 18:23:44 +0800

    3. Price and Date Formatting 3.1-3.2

--- viewproduct.php
+++ viewproduct.php
@@ -1,8 +1,8 @@
 <label for="">Product Name: <?php echo $_POST["product_name"]; ?> </label> <br>
 <label for="">Category: <?php echo $_POST["category"]; ?> </label> <br>
-<label for="">Price: <?php echo $_POST["price"]; ?> </label> <br>
+<label for="">Price: <?php echo number_format($_POST["price"]); ?> </label> <br>
 <label for="">Stock Quantity: <?php echo $_POST["stock_quantity"]; ?> </label> <br>
-<label for="">Expiration Date: <?php echo $_POST["expiration_date"]; ?> </label> <br>
+<label for="">Expiration Date: <?php echo date("M-d-Y", strtotime($_POST["expiration_date"])); ?> </label> <br>
 <label for="">Status: <?php echo $_POST["status"]; ?> </label> <br>
 
 
